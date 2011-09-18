86113d15dd2e1edda24912e095fb4e02e8592352
dmitryhd <dmitryhd@gmail.com>
2011-09-18 22:00:30 +0400

    all tests passed

--- freqdict.cc
+++ freqdict.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "freqdict.h"
 int ch;
 char next_word[MAX_WORD];
@@ -21,20 +22,30 @@ char *getNextWord(FILE *file) {
 	int i = 0;
 
 	printf("MOCK: calling getNextWord\n");
-
+  char is_word_begin = 0;
 	//next_word = (char*) malloc(50);
 	while (true) {
 		ch = getc(file);
-		if (ch == EOF)
-			// exit(1);
-			return next_word;
-
-		if ((ch != '\n') && (ch != '\t') && (ch != ' ') && (ch != '\r'))
+    // printf("getNW: i=%d, get=%c\n", i, ch);
+    if (isspace(ch)) {
+      if (is_word_begin) {
+        // printf("getNW: endword i=%d, get=%c\n", i, ch);
+        next_word[i] = '\0';
+        return next_word;
+      }
+      else
+        continue;
+    }
+    else {
+      is_word_begin = 1;
 			next_word[i++] = ch;
-		else {
+    }
+		if (ch == EOF) {
+			// exit(1);
+      // printf("getNW: i=%d EOF\n", i);
 			next_word[i] = '\0';
 			return next_word;
-		}
+    }
 	}
 }
 
--- freqdict_unittest.cc
+++ freqdict_unittest.cc
@@ -4,9 +4,12 @@
 // openFile():
 TEST(openFileTest, validFile) {
   // TODO(dmitryhd): add here error handling
-  FILE * new_f = fopen("/tmp/openfiletest_simple", "w+");
+  char filename_const[]= "/tmp/openfiletest_simple";
+  char * filename = &filename_const[0];
+
+  FILE * new_f = fopen(filename_const, "w+");
   fprintf(new_f, "omg\n");
-  FILE * open_file = openFile("/tmp/openfiletest_simple");
+  FILE * open_file = openFile(filename);
   FILE * wrong_file = 0;
   ASSERT_NE(open_file, wrong_file);
   fclose(new_f);
@@ -16,27 +19,37 @@ TEST(openFileTest, validFile) {
 // getNextWord():
 TEST(GetWordTest, SimpleSpaces) {
   // TODO(dmitryhd): add here error handling
-  FILE * new_f = fopen("/tmp/getwordtest_simple\n", "w+");
+  char filename_const[]= "/tmp/SimpleSpaces";
+  char * filename = &filename_const[0];
+
+  FILE * new_f = fopen(filename_const, "w+");
   fprintf(new_f, "omg test\n");
-  FILE * open_file = openFile("/tmp/openfiletest_simple");
+  fclose(new_f);
+  FILE * open_file = openFile(filename);
   char * x = getNextWord(open_file);
-  printf("1: x = %s\n", x);
+  // printf("1: x = %s\n", x);
   EXPECT_STREQ("omg", x);
   x = getNextWord(open_file);
-  printf("2: x = %s\n", x);
+  // printf("2: x = %s\n", x);
   EXPECT_STREQ("test", x);
-  fclose(new_f);
+  fclose(open_file);
 }
 
 TEST(GetWordTest, ComplicatedSpaces) {
   // TODO(dmitryhd): add here error handling
-  FILE * new_f = fopen("/tmp/getwordtest_complicated\n", "w+");
-  fprintf(new_f, " \t\n    omg  \n\t t   \t\n est\n");
-  EXPECT_STREQ("omg", getNextWord(new_f));
-  EXPECT_STREQ("t", getNextWord(new_f));
-  EXPECT_STREQ("ets", getNextWord(new_f));
+  char filename_const[]= "/tmp/openfiletest_simple";
+  char * filename = &filename_const[0];
+
+  FILE * new_f = fopen(filename_const, "w+");
+  fprintf(new_f, "omg  \n\t t   \t\n est\n");
+  fclose(new_f);
+  FILE * open_file = openFile(filename);
+  EXPECT_STREQ("omg", getNextWord(open_file));
+  EXPECT_STREQ("t", getNextWord(open_file));
+  EXPECT_STREQ("est", getNextWord(open_file));
   fclose(new_f);
 }
+
 // there is no need for main().
 // main resides in gtest_main.a see Makefile
 // main contains only RUN_ALL_TESTS()
